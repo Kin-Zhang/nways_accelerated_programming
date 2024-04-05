2c59103186646cb75f0a62f1ef885e935f6e735c
Kin-home <kinzhangglimmer@gmail.com>
2024-04-06 00:58:07 +0200

    style: swap with func

--- _challenge/C/source_code/serial/cfd.cpp
+++ _challenge/C/source_code/serial/cfd.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
+#include <queue>
 #include "arraymalloc.h"
 #include "boundary.h"
 #include "jacobi.h"
@@ -219,9 +219,7 @@ int main(int argc, char **argv)
       //copy back
       nvtxRangePush("Switch_Array");
 
-    double *tmp = psi;
-    psi = psitmp;
-    psitmp = tmp;
+    std::swap(psi, psitmp);
 
       if (!irrotational)
 	{
